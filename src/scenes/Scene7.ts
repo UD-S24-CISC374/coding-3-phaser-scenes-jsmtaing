d93232f67b46096355ab9072d91133d00f74c122
jsmtaing <jostai2003@gmail.com>
2024-03-07 23:40:13 -0500

    Edited final scene

--- src/scenes/Scene7.ts
+++ src/scenes/Scene7.ts
@@ -19,14 +19,14 @@ export default class Scene7 extends Phaser.Scene {
         this.cameras.main.fadeIn(Constants.FADE_DURATION, Constants.FADE_COLOR_RED, Constants.FADE_COLOR_GREEN, Constants.FADE_COLOR_BLUE);
         this.add.text(
             this.cameras.main.displayWidth * Phaser.Math.FloatBetween(0.625, 0.675),
-            this.cameras.main.displayHeight * (0.5),
+            this.cameras.main.displayHeight * (0.4),
             `Amount Kiki'd: ${this.state.kiki}`,
             { color: '#2B2F33', fontSize: '50px'}
         );
 
         this.add.text(
             this.cameras.main.displayWidth * Phaser.Math.FloatBetween(0.625, 0.675),
-            this.cameras.main.displayHeight * (0.5),
+            this.cameras.main.displayHeight * (0.6),
             `Amount Bouba'd: ${this.state.bouba}`,
             { color: '#2B2F33', fontSize: '50px'}
         );
