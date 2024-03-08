8d4578428d39d412bf9dcd9ecf08f3ddfe2fc025
jsmtaing <jostai2003@gmail.com>
2024-03-07 23:04:30 -0500

    Test 3

--- src/scenes/Scene0.ts
+++ src/scenes/Scene0.ts
@@ -12,7 +12,7 @@ export default class Scene0 extends Phaser.Scene {
     }
 
     create() {
-        const welcome = this.add.text(this.cameras.main.displayWidth * (1/2), this.cameras.main.DisplayHeight * (4/10), 'Select *KIKI* or *BOUBA* to start!');
+        this.add.text(this.cameras.main.displayWidth * (1/2), this.cameras.main.DisplayHeight * (4/10), 'Select *KIKI* or *BOUBA* to start!');
 
         const kikiBtn = this.add.text(this.cameras.main.displayWidth * (1/3), this.cameras.main.DisplayHeight * (7/10), 'KIKI', { color: '#2B2F33', fontSize: '36px'}).setInteractive();
         kikiBtn.on('pointerdown', () => {
